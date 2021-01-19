97339b73d41a7a6d42979be9ad36ac5efa04fb4e
Saurav Das <saurav.das37@gmail.com>
2021-01-19 22:58:45 +0530

    using modals as confirmation boxes

--- static/scripts.js
+++ static/scripts.js
@@ -6,41 +6,71 @@ $("#new-category").change(function() {
   }
 });
 
-$(".deletePalBtn").click(function() {
+$("#staticBackdrop").on("show.bs.modal", function(e) {
   $(".selectStampsList").removeClass("bg-secondary");
   $(".selectStampsList").removeClass("text-white");
   $(".selectStampsList").addClass("text-dark");
-  
-  let id = $(this).attr("id");
-  let userId = id.substring(10);
-  $("#user-detail-alert-text").html("Are you sure you wish to delete this user? All data will be lost forever!");
-  $("#user-detail-alert-confirm").attr("href", `/delete/${userId}`);
-  $("#user-detail-alert").removeClass("d-none");
-});
-
-$("#user-detail-alert-close").click(function() {
-  $("#user-detail-alert").addClass("d-none");
-
-  $(".selectStampsList").removeClass("bg-secondary");
-  $(".selectStampsList").removeClass("text-white");
-  $(".selectStampsList").addClass("text-dark");
-});
-
-$(".selectStampsList").click(function() {
-  let id = $(this).attr("id");
-  let stampID = id.substring(12);
 
   let uid = $(".deletePalBtn").attr("id");
   let userId = uid.substring(10);
+  let userName = $(".user-name").html();
 
+  if ($(e.relatedTarget).hasClass("deletePalBtn")) {
+    let imageHref = "/static/user-placeholder.jpg";
+
+    let uImageId = $(".user-image").attr("id");
+    if (uImageId != undefined) {
+      let userImageId = uImageId.substring(5);
+      imageHref = `/user_image/${userImageId}`;
+    }
+
+    $("#common-modal-body").html(`
+      <table>
+        <tr>
+          <td>
+            <img src="${imageHref}" height="100" width="100">
+          </td>
+          <td  style="padding:20px">
+            <span>Are you sure you wish to delete <b>${userName}</b>? Beware, all data will be lost forever!</span>
+          </td>
+        </tr>
+      </table>`);
+    $("#common-modal-confirm-btn").removeClass("btn-primary").addClass("btn-danger");
+    $("#common-modal-confirm-link").attr("href", `/delete/${userId}`);
+
+  } else if ($(e.relatedTarget).hasClass("selectStampsList")) {
+    let id = e.relatedTarget.id;
+    let stampID = id.substring(12);
+
+    let stampHref = `/static/stamp-placeholder.png`;
+    let sImageId = $(e.relatedTarget).children("img").attr("id");
+    if (sImageId != undefined ) {
+      let stampImageId = sImageId.substring(6);
+      stampHref = `/stamp_image/${stampImageId}`;
+    }
+    let stampName = $(e.relatedTarget).find("h5").html();
+
+    $(e.relatedTarget).removeClass("text-dark");
+    $(e.relatedTarget).addClass("text-white");
+    $(e.relatedTarget).addClass("bg-secondary");
+
+    $("#common-modal-body").html(`
+      <table>
+        <tr>
+          <td>
+            <img src="${stampHref}" height="100" width="100">
+          </td>
+          <td  style="padding:20px">
+            <span>Are you sure you wish to use stamp <b>${stampName}</b> for your pal <b>${userName}</b></span>?
+          </td>
+        </tr>
+      </table>`);
+    $("#common-modal-confirm-link").attr("href", `/use/${userId}-${stampID}`);
+  }
+});
+
+$("#common-modal-cancel-btn").click(function() {
   $(".selectStampsList").removeClass("bg-secondary");
   $(".selectStampsList").removeClass("text-white");
   $(".selectStampsList").addClass("text-dark");
-  $(this).removeClass("text-dark");
-  $(this).addClass("text-white");
-  $(this).addClass("bg-secondary");
-
-  $("#user-detail-alert-text").html("Do you wish to use the selected stamp for this user?");
-  $("#user-detail-alert-confirm").attr("href", `/use/${userId}-${stampID}`);
-  $("#user-detail-alert").removeClass("d-none");
 });
